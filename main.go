50f93857f1d4506bb23a1ffd51de6830a231cb5d
terrywh <terry.wuhao@gmail.com>
2018-05-21 21:40:35 +0800

    日志输出说明

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	flag.StringVar(&address, "bind", ":8080", "此代理程序监听的地址端口")
 	flag.Var(&suffixs, "suffix", "允许通过进行透明代理的目标域名结尾, 可进行多次指定; 默认允许所有域名通过")
 	flag.Var(&origins, "origin", "允许通过进行透明代理的源域名结尾, 设置或替换代理目标 CORS 配置 (Access-Control-Allow-Origin), 可以多次指定")
-	flag.StringVar(&logpath, "log", "", "重定向日志输出到指定文件, 不指定时输出到标准输出")
+	flag.StringVar(&logpath, "log", "", "重定向日志输出到指定文件 (发送 SIGUSR2 信号重载)")
 	flag.Parse()
 	if help {
 		flag.PrintDefaults()
